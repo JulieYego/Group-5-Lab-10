e0612c2bdfb9d5b9fe32dba6a6d4e0bda47260a7
JulieYego <77244540+JulieYego@users.noreply.github.com>
2021-04-01 15:27:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<link href="css/style.css" rel="stylesheet" type="text/css"/>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro|Open+Sans+Condensed:300|Raleway' rel='stylesheet' type='text/css'>
+<title>Export Google Contacts Using PHP</title>
+
+</head>
+<body>
+
+<div class="col-sm-6 col-md-4 col-lg-2">
+<div class="col-sm-6 col-md-4 col-lg-2">
+
+<header class="col-sm-6 col-md-4 col-lg-2">
+<center>
+<h2>Export Gmail Contacts With Google Oauth PHP</h2>
+</center>
+</header>
+<hr>
+<div  class="col-sm-6 col-md-4 col-lg-2">
+<center>
+<h3 id ="sign">Sign In with Google for retrieving Contacts</h3>
+<div class="col-sm-6 col-md-4 col-lg-2">
+<a href="https://accounts.google.com/o/oauth2/auth?client_id=1067667438304-8e5hqhvjnd0r51jd6rrtofh1buenkup4.apps.googleusercontent.com&redirect_uri=http://localhost/gmailcontact/callback.php&scope=https://www.google.com/m8/feeds/&response_type=code">Export from Gmail</a>
+</div>
+</center>
+</div>
+</div>
+</div>
+
+</body>
+</html>
+
